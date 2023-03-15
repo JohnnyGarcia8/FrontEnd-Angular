e0099276b60d67b8cc0fff366427ac4f3bd1fdde
Jonathan García <jonibgarcia@gmail.com>
2023-03-15 19:22:25 -0300

    Cree carpeta dashboard, error, inicio, login y secciones para ordenar los componentes ya creados (basado en el primer modelo portfolio web)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { DashboardComponent } from './Dashboard/dashboard/dashboard.component';
+import { ErrorComponent } from './error/error.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {path: '', component:InicioComponent},
+  {path:'dashboard', component:DashboardComponent},
+  {path:'**', component:ErrorComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,58 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-import { ProyectosComponent } from './proyectos/proyectos.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { DashboardComponent } from './Dashboard/dashboard/dashboard.component';
+import { ErrorComponent } from './error/error.component';
+
+
 import { NavbarComponent } from './navbar/navbar.component';
-import { AcercadeComponent } from './acercade/acercade.component';
-import { ExperienciaComponent } from './experiencia/experiencia.component';
-import { EducacionComponent } from './educacion/educacion.component';
-import { HabilidadesComponent } from './habilidades/habilidades.component';
-import { FooterComponent } from './footer/footer.component';
+import { AcercadeComponent } from './Secciones/acercade/acercade.component';
+import { ExperienciaComponent } from './Secciones/experiencia/experiencia.component';
+import { EducacionComponent } from './Secciones/educacion/educacion.component';
+import { HabilidadesComponent } from './Secciones/habilidades/habilidades.component';
+import { ProyectosComponent } from './Secciones/proyectos/proyectos.component';
+import { FooterComponent } from './Secciones/footer/footer.component';
 import { BannerComponent } from './banner/banner.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { PerfilComponent } from './perfil/perfil.component';
+import { LoginComponent } from './login/login.component';
+
+
+
+
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProyectosComponent,
+    InicioComponent,
+    DashboardComponent,
+    ErrorComponent,
+    
+    
     NavbarComponent,
     AcercadeComponent,
     ExperienciaComponent,
     EducacionComponent,
     HabilidadesComponent,
+    ProyectosComponent,
     FooterComponent,
     BannerComponent,
-    DashboardComponent
+    PerfilComponent,
+    LoginComponent,
+    
   ],
+
   imports: [
     BrowserModule,
     AppRoutingModule,
   ],
+
   providers: [],
   bootstrap: [AppComponent]
+
 })
 export class AppModule { }
