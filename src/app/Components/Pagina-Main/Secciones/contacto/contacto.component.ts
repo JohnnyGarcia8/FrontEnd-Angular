b7b4933fe7c95014372bad99a5dfe5a1e327f69b
Jonathan García <jonibgarcia@gmail.com>
2023-05-19 17:25:36 -0300

    Ordeno codigo html (secciones)

--- src/app/Components/Pagina-Main/Secciones/contacto/contacto.component.ts
+++ src/app/Components/Pagina-Main/Secciones/contacto/contacto.component.ts
@@ -11,20 +11,20 @@ import { LoginServiceService } from 'src/assets/services/servicio/login-service.
 })
 export class ContactoComponent {
   constructor(
-    public loginService:LoginServiceService,
-    private contactoServices: ContactoService){ 
+    public loginService: LoginServiceService,
+    private contactoServices: ContactoService) {
   }
 
-  onEdition:boolean=false;
+  onEdition: boolean = false;
   contactos: Contacto[] = [];
 
 
-  ngOnInit(){
-    this.loginService.observableEdit.subscribe(respuesta=>{this.onEdition=respuesta});
+  ngOnInit() {
+    this.loginService.observableEdit.subscribe(respuesta => { this.onEdition = respuesta });
     this.onGet();
   }
 
-  onGet(){
-    this.contactoServices.getContacto().subscribe(data =>{this.contactos=data});
+  onGet() {
+    this.contactoServices.getContacto().subscribe(data => { this.contactos = data });
   }
 }
